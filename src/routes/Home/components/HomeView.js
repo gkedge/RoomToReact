4c48ac17969a31d8121ea9496f87032179156583
Greg Kedge <greg.kedge@atsid.com>
2016-05-21 15:59:22 -0400

    Revert "Merge branch 'master' of https://github.com/atsid/RoomToReact"
    This reverts commit adbc65136a1dc76ebcdbc7b0ea22e02db09bef94, reversing
    changes made to 06d0506041448b399a7226b011da5a62b61e381c.

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import DuckImage from '../assets/Duck.jpg'
+import classes from './HomeView.scss'
 
 export const HomeView = () => (
   <div>
-    <h2>Home View</h2>
+    <h4>Welcome!</h4>
+    <img
+      alt='This is a duck, because Redux!'
+      className={classes.duck}
+      src={DuckImage} />
   </div>
 )
 
